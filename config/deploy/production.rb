594126caa8d992da651c941691248702f0d430d8
Yogesh Waghmare <ywaghmare5203@gmail.com>
2019-07-08 12:13:59 +0530

    add script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -62,3 +62,23 @@
 
 
 role :web, %w{root@192.99.54.120}
+
+# Define roles, user and IP address of deployment server
+# role :name, %{[user]@[IP adde.]}
+#role :app, %w{deployer@162.243.74.190}
+
+# Define server(s)
+# Example:
+# server '[your droplet's IP addr]', user: '[the deployer user]', roles: %w{[role names as defined above]}
+# server '162.243.74.190', user: 'deployer', roles: %w{app}
+server '192.99.54.120', user: 'root', roles: %w{app}
+
+# SSH Options
+# See the example commented out section in the file
+# for more options.
+set :ssh_options, {
+    forward_agent: false,
+    auth_methods: %w(password),
+    password: 'C0n$@g0u5!',
+    user: 'root',
+}
